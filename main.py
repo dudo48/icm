5ac391d18d91f2d1da958f33db0f858e321d6466
dudo48 <test>
2021-02-02 06:38:56 +0200

    1.0

---
+++ main.py
@@ -0,0 +1,162 @@
+import csv
+import datetime
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.common.by import By
+import time
+import credentials
+import sys
+
+PACKAGE_SIZE = 140  # data units
+MONTH = 30  # days
+LOGIN_URL = "https://my.te.eg/#/home/signin"
+USAGE_URL = "https://my.te.eg/#/offering/usage"
+DAYS_LEFT_SELECTOR = "div.row:nth-child(4) > div:nth-child(2)"
+PACKAGE_SIZE_SELECTOR = ".text-center > strong:nth-child(2)"
+REMAINING_UNITS_SELECTOR = "span.text-dir:nth-child(4)"
+DELIMITER = ','
+TIMEOUT = 120  # time to wait for page to load the required element
+REPORT_ROW_WIDTH = 40  # spaces to leave after each entry in report
+MAXIMUM_TRIES = 5       # maximum amount to retry to get the record in case of failure
+
+
+def type_slowly(browser, element_id, text, delay):
+    element = WebDriverWait(browser, TIMEOUT).until(
+        expected_conditions.presence_of_element_located((By.ID, element_id))
+    )
+    for char in text:
+        element.send_keys(char)
+        time.sleep(delay)
+
+
+def create_record():
+    today_date = datetime.datetime.now()
+    date = str(today_date.day).zfill(2) + '/' + str(today_date.month).zfill(2) + '/' + str(today_date.year).zfill(4)
+
+    # log in and get required data
+    options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_experimental_option("excludeSwitches", ["enable-logging"])
+    options.add_argument("--log-level=3")
+
+    browser = webdriver.Chrome(options=options)
+    browser.get(LOGIN_URL)
+
+    try:
+        type_slowly(browser, "MobileNumberID", credentials.USERNAME, 0.1)
+        type_slowly(browser, "PasswordID", credentials.PASSWORD, 0.1)
+        sign_in_element = WebDriverWait(browser, TIMEOUT).until(
+            expected_conditions.presence_of_element_located((By.ID, "singInBtn"))
+        )
+        sign_in_element.click()
+        time.sleep(5)
+        browser.get(USAGE_URL)
+
+        days_left_element = WebDriverWait(browser, TIMEOUT).until(
+            expected_conditions.presence_of_element_located((By.CSS_SELECTOR, DAYS_LEFT_SELECTOR))
+        )
+        package_size_element = WebDriverWait(browser, TIMEOUT).until(
+            expected_conditions.presence_of_element_located((By.CSS_SELECTOR, PACKAGE_SIZE_SELECTOR))
+        )
+        remaining_units_element = WebDriverWait(browser, TIMEOUT).until(
+            expected_conditions.presence_of_element_located((By.CSS_SELECTOR, REMAINING_UNITS_SELECTOR))
+        )
+
+        days_left = int(days_left_element.text.split(' ')[0])
+        package_size = float(package_size_element.text.split(' ')[3])
+        consumed_units = float(package_size_element.text.split(' ')[0])
+        remaining_units = float(remaining_units_element.text.split(' ')[1])
+        browser.quit()
+
+    except Exception:
+        # entered in case of failure of the program(like element not found)
+        browser.quit()
+        return []
+
+    consumption_in_between = round(float(last_day_data["remaining units"]) - remaining_units, 2)
+    last_date = datetime.datetime.strptime(last_day_data["date"], "%d/%m/%Y")   # string to datetime object
+    # new monthly package started
+    if days_left > int(last_day_data["days left"]) or abs(today_date - last_date).days >= MONTH:
+        consumption_in_between = round(PACKAGE_SIZE - remaining_units, 2)
+
+    consumed_percentage = round(float(consumed_units) / float(package_size), 3)
+    projected_consumption = round(remaining_units / days_left, 2)
+    average_consumption = round(consumed_units / (MONTH - days_left + 1), 2)
+
+    new_row = []
+    new_row.append(date)
+    new_row.append(str(days_left))
+    new_row.append(str(package_size))
+    new_row.append(str(consumed_units))
+    new_row.append(str(consumed_percentage))
+    new_row.append(str(remaining_units))
+    new_row.append(str(consumption_in_between))
+    new_row.append(str(projected_consumption))
+    new_row.append(str(average_consumption))
+
+    return new_row
+
+
+def add_to_database(new_row):
+    with open("database.csv", mode='a') as writer:
+        writer.write('\n' + new_row)
+        writer.close()
+
+
+def create_today_report(row):
+    today_report = ""
+
+    for i in range(len(table_headers)):
+        table_headers[i] = table_headers[i].title()  # capitalize each word of the string
+        today_report += table_headers[i] + ":" + (" " * (REPORT_ROW_WIDTH - len(table_headers[i]))) + row[i]
+
+        if i != len(table_headers) - 1:
+            today_report += '\n'
+
+    # write it to file
+    with open("report.txt", mode='w') as writer:
+        writer.write(today_report)
+        writer.close()
+
+
+# saves today data to separate file for quick access the next run of the program
+def set_last_day_data(today_data):
+    with open("last_day_data.csv", mode='w') as writer:
+        writer.write(today_data)
+        writer.close()
+
+
+def main():
+    row = []
+    for tries in range(MAXIMUM_TRIES):
+        row = create_record()
+        if row:     # if list isn't empty
+            break
+        if tries == MAXIMUM_TRIES - 1:
+            sys.exit(1)     # used up all tries and failed
+        time.sleep(10)      # wait some time before retrying
+
+    row_string = DELIMITER.join(row)
+    add_to_database(row_string)
+    set_last_day_data(row_string)
+    create_today_report(row)
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    # get table headers
+    with open("database.csv", mode='r') as database:
+        csv_reader = csv.reader(database, delimiter=DELIMITER)
+        table_headers = next(csv_reader)
+        database.close()
+
+    # access last day data
+    with open("last_day_data.csv", mode='r') as last_data:
+        csv_reader = csv.reader(last_data, delimiter=DELIMITER)
+        last_day_data = dict(zip(table_headers, next(csv_reader)))
+        last_data.close()
+
+    main()
