83d909e7fb2e10c4a1564a5ecd44cea2c86e428c
dudo48 <test>
2021-04-02 21:35:28 +0200

    1.0

--- scheduler.py
+++ scheduler.py
@@ -1,8 +1,25 @@
-import main
+import icm
+import time
+import datetime
+
+CHECK_PERIOD = 5  # minutes
+START_HOUR = 22
+END_HOUR = 0
+
+
+def in_hour_range(start_hour, end_hour, current_hour):
+    if start_hour < end_hour:
+        return start_hour <= current_hour < end_hour
+    else:
+        return current_hour >= start_hour or current_hour < end_hour
 
 
 def main():
-    pass
+    while True:
+        hour = datetime.datetime.now().hour
+        if in_hour_range(START_HOUR, END_HOUR, hour):
+            icm.run()
+        time.sleep(CHECK_PERIOD * 60)
 
 
 if __name__ == '__main__':
--- testing.py
+++ testing.py
@@ -1,4 +1,4 @@
-from main import add_to_sql_database
+from icm import add_to_sql_database
 
 l = ['2021-02-02', '14', '140.0', '61.06', '0.436', '78.94', '2.11', '5.64', '3.59']
 add_to_sql_database(tuple(l))
