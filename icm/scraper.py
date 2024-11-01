b2ec695d936c49316cb926d552bf14936840f8cd
Fady Emad <fadyemad14705@gmail.com>
2024-11-01 15:32:01 +0200

    improve: scraper

--- icm/scraper.py
+++ icm/scraper.py
@@ -2,6 +2,7 @@ import contextlib
 import dataclasses
 import datetime
 import os
+import re
 import time
 from typing import Optional
 
@@ -50,6 +51,10 @@ def login(headless: bool = True):
 @dataclasses.dataclass
 class Scraper:
     browser: WebDriver
+    timeout: float = 60
+
+    def __post_init__(self):
+        self.browser.implicitly_wait(self.timeout)
 
     @classmethod
     def from_options(cls, options: Optional[Options] = None):
@@ -58,86 +63,53 @@ class Scraper:
         service = Service(ChromeDriverManager().install())
         return cls(webdriver.Chrome(service=service, options=options))
 
-    @staticmethod
-    def wait():
-        return time.sleep(config["scraper"]["waiting_time"])
+    @property
+    def wait(self):
+        return WebDriverWait(self.browser, self.timeout)
 
     def _get_renewal_date(self):
         self.browser.get(url.OVERVIEW)
-        self.wait()
-
-        renewal_date_element = self.browser.find_element(
-            By.CSS_SELECTOR, css_selector.RENEWAL_DATE
-        )
-        renewal_date = datetime.datetime.strptime(renewal_date_element.text.split()[2][:-1], "%d-%m-%Y")
-        return renewal_date
+        renewal_date_element = self.browser.find_element(By.CSS_SELECTOR, css_selector.RENEWAL_DATE)
+        renewal_date_text: str = self.wait.until(
+            lambda _: re.findall(r"\d{2}-\d{2}-\d{4}", renewal_date_element.text)
+        )[0]
+        return datetime.datetime.strptime(renewal_date_text, "%d-%m-%Y")
 
     def _get_consumed_units(self):
         self.browser.get(url.USAGE)
-        self.wait()
-
-        consumed_units_element = self.browser.find_element(
-            By.CSS_SELECTOR, css_selector.CONSUMED_UNITS
-        )
+        consumed_units_element = self.browser.find_element(By.CSS_SELECTOR, css_selector.CONSUMED_UNITS)
         consumed_units = float(consumed_units_element.text.split()[0])
         return consumed_units
 
     def _get_remaining_units(self):
         self.browser.get(url.USAGE)
-        self.wait()
-
-        remaining_units_element = self.browser.find_element(
-            By.CSS_SELECTOR, css_selector.REMAINING_UNITS
-        )
+        remaining_units_element = self.browser.find_element(By.CSS_SELECTOR, css_selector.REMAINING_UNITS)
         remaining_units = float(remaining_units_element.text.split()[0])
         return remaining_units
 
     def login(self):
         self.browser.get(url.LOGIN)
-        timeout: int = config["scraper"]["timeout"]
 
         # type service number
-        service_number_element = WebDriverWait(self.browser, timeout).until(
-            expected_conditions.visibility_of_element_located(
-                (By.CSS_SELECTOR, css_selector.SERVICE_NUMBER)
-            )
-        )
+        service_number_element = self.browser.find_element(By.CSS_SELECTOR, css_selector.SERVICE_NUMBER)
         service_number_element.click()
         type_slowly(service_number_element, config["credentials"]["number"])
 
         # select service type
-        service_type_element = WebDriverWait(self.browser, timeout).until(
-            expected_conditions.visibility_of_element_located(
-                (By.CSS_SELECTOR, css_selector.SERVICE_TYPE)
-            )
-        )
+        service_type_element = self.browser.find_element(By.CSS_SELECTOR, css_selector.SERVICE_TYPE)
         service_type_element.click()
-        internet_service_type_element = WebDriverWait(self.browser, timeout).until(
-            expected_conditions.visibility_of_element_located(
-                (By.CSS_SELECTOR, css_selector.INTERNET_SERVICE_TYPE)
-            )
-        )
+        internet_service_type_element = self.browser.find_element(By.CSS_SELECTOR, css_selector.INTERNET_SERVICE_TYPE)
         internet_service_type_element.click()
 
         # type password
-        password_element = WebDriverWait(self.browser, timeout).until(
-            expected_conditions.visibility_of_element_located(
-                (By.CSS_SELECTOR, css_selector.PASSWORD)
-            )
-        )
+        password_element = self.browser.find_element(By.CSS_SELECTOR, css_selector.PASSWORD)
         password_element.click()
         type_slowly(password_element, config["credentials"]["password"])
 
         # click log in button
-        sign_in_element = WebDriverWait(self.browser, timeout).until(
-            expected_conditions.visibility_of_element_located(
-                (By.CSS_SELECTOR, css_selector.SIGN_IN_BUTTON)
-            )
-        )
+        sign_in_element = self.browser.find_element(By.CSS_SELECTOR, css_selector.SIGN_IN_BUTTON)
         sign_in_element.click()
-        WebDriverWait(self.browser, timeout).until(
-            expected_conditions.url_to_be(url.INDEX)
-        )
+        self.wait.until(expected_conditions.url_to_be(url.INDEX))
 
     def create_record(self):
         date = datetime.datetime.now()
