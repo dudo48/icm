9dc4b476f67b5723cd275802944b5bad2d7600b1
Fady Emad <fadyemad14705@gmail.com>
2024-11-03 22:05:40 +0200

    improve: unify database and log folders

--- icm/path.py
+++ icm/path.py
@@ -2,17 +2,14 @@ from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).parent.parent
 
-DATABASE_FOLDER = PROJECT_ROOT.joinpath("database")
-LOG_FOLDER = PROJECT_ROOT.joinpath("log")
-
-DATABASE_FOLDER.mkdir(exist_ok=True)
-LOG_FOLDER.mkdir(exist_ok=True)
-
-DATABASE = DATABASE_FOLDER.joinpath("database.db")
-NEXT_RUN = DATABASE_FOLDER.joinpath("next_run")
-
-LOG = LOG_FOLDER.joinpath("log.txt")
-REPORT = LOG_FOLDER.joinpath("report.txt")
-
 CONFIG = PROJECT_ROOT.joinpath("config.toml")
 CONFIG_EXAMPLE = PROJECT_ROOT.joinpath("config.example.toml")
+
+DATA_FOLDER = PROJECT_ROOT.joinpath("data")
+DATA_FOLDER.mkdir(exist_ok=True)
+
+DATABASE = DATA_FOLDER.joinpath("database.db")
+NEXT_RUN = DATA_FOLDER.joinpath("next_run")
+
+LOG = DATA_FOLDER.joinpath("log.txt")
+REPORT = DATA_FOLDER.joinpath("report.txt")
