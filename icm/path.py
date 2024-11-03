3612576d4d3764e3412ef74b1782a8fbf15ff118
Fady Emad <fadyemad14705@gmail.com>
2024-11-03 17:01:11 +0200

    improve .gitignore

--- icm/path.py
+++ icm/path.py
@@ -2,14 +2,14 @@ from pathlib import Path
 
 PROJECT_ROOT = Path(__file__).parent.parent
 
-DATABASES = PROJECT_ROOT.joinpath("databases")
+DATABASE_FOLDER = PROJECT_ROOT.joinpath("database")
 LOG_FOLDER = PROJECT_ROOT.joinpath("log")
 
-DATABASES.mkdir(exist_ok=True)
+DATABASE_FOLDER.mkdir(exist_ok=True)
 LOG_FOLDER.mkdir(exist_ok=True)
 
-DATABASE = DATABASES.joinpath("database.db")
-NEXT_RUN = DATABASES.joinpath("next_run")
+DATABASE = DATABASE_FOLDER.joinpath("database.db")
+NEXT_RUN = DATABASE_FOLDER.joinpath("next_run")
 
 LOG = LOG_FOLDER.joinpath("log.txt")
 REPORT = LOG_FOLDER.joinpath("report.txt")
