836460fc06e6247b75643469496aca17a3dc64be
Fady Emad <fadyemad14705@gmail.com>
2024-11-08 17:51:05 +0200

    improve: Record __str__

--- icm/record.py
+++ icm/record.py
@@ -24,25 +24,18 @@ class Record(Base):
         return f"{self.__class__.__name__}({fields_info})"
 
     def __str__(self) -> str:
-        keys = [
-            "Date:",
-            "Renewal Date:",
-            "Remaining Units:",
-            "Consumed Units:",
-            "Projected Consumption (Daily):",
-        ]
-        values = [
-            self.date.strftime(DATETIME_FORMAT),
-            self.renewal_date.strftime(DATETIME_FORMAT),
-            f"{self.remaining_units:.2f}",
-            f"{self.consumed_units:.2f}",
-            f"{self.projected_consumption:.2f}",
-        ]
+        data_dict = {
+            "Date": self.date.strftime(DATETIME_FORMAT),
+            "Renewal Date": self.renewal_date.strftime(DATETIME_FORMAT),
+            "Remaining Units": f"{self.remaining_units:.2f}",
+            "Consumed Units": f"{self.consumed_units:.2f}",
+            "Projected Consumption (Daily)": f"{self.projected_consumption:.2f}",
+        }
 
-        key_align = len(max(keys, key=len)) + 1
-        value_align = len(max(values, key=len))
+        key_align = len(max(data_dict.keys(), key=len)) + 1
+        value_align = len(max(data_dict.values(), key=len))
         return "\n".join(
-            k.ljust(key_align) + v.rjust(value_align) for k, v in zip(keys, values)
+            (k + ":").ljust(key_align) + v.rjust(value_align) for k, v in data_dict.items()
         )
 
     @property
