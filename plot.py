ed602c4be741a361be66f40a44558104a327b2da
Fady Emad <fadyemad14705@gmail.com>
2024-11-11 17:25:28 +0200

    improve (plot): seperate records dataframes by package

--- plot.py
+++ plot.py
@@ -14,34 +14,32 @@ from icm.utility import DATETIME_FORMAT
 T = TypeVar("T")
 
 
-def get_dataframe(query: Select[tuple[Record]]):
-    def calculate_average(record: pd.Series):
-        first = first_records.loc[record["renewal_date"]]
-        record["average_daily_consumed_units"] = (
-            record["consumed_units"] - first["consumed_units"]
-        ) / (first["days_left"] - record["days_left"])
-        return record
-
+def get_dataframes(query: Select[tuple[Record]]):
     query = query.order_by(Record.date)
-    records = pd.read_sql(query, engine)
-    records["time_left"] = records["renewal_date"] - records["date"]
-    records["delta_consumed_units"] = records["consumed_units"].diff()
-    records["delta_date"] = records["date"].diff()
-    records["projected_delta_consumed_units"] = records["remaining_units"] * (records["delta_date"] / records["time_left"])
-    records["projected_delta_consumed_units_ratio"] = records["delta_consumed_units"] / records["projected_delta_consumed_units"]
-    records["days_left"] = records["time_left"] / pd.to_timedelta(1, "D")
-    records["projected_daily_consumed_units"] = records["remaining_units"] / records["days_left"]
-    first_records = records.groupby("renewal_date").first()
-    records = records.apply(calculate_average, axis=1)
-    return records
+    dataframes: list[pd.DataFrame] = []
+    for _, df in pd.read_sql(query, engine).groupby("renewal_date"):
+        df["time_left"] = df["renewal_date"] - df["date"]
+        df["delta_consumed_units"] = df["consumed_units"].diff()
+        df["delta_date"] = df["date"].diff()
+        df["projected_delta_consumed_units"] = df["remaining_units"] * (df["delta_date"] / df["time_left"])
+        df["projected_delta_consumed_units_ratio"] = df["delta_consumed_units"] / df["projected_delta_consumed_units"]
+        df["days_left"] = df["time_left"] / pd.to_timedelta(1, "D")
+        df["projected_daily_consumed_units"] = df["remaining_units"] / df["days_left"]
+        first = df.iloc[0]
+        df["average_daily_consumed_units"] = (
+            df["consumed_units"] - first["consumed_units"]
+        ) / (first["days_left"] - df["days_left"])
+        dataframes.append(df)
+
+    return dataframes
 
 
-def show_plot(records: pd.DataFrame):
+def show_plot(records_list: list[pd.DataFrame]):
     figure, axes = cast(tuple[Figure, Sequence[Axes]], plot.subplots(2, sharex=True))
     colormap = LinearSegmentedColormap.from_list("", ["green", "yellow", "red"], 16)
+    records = records_list[-1]
 
     axes[0].plot(records["date"], records["remaining_units"], color="blue", marker="o", label="Remaining units")
-    axes[0].legend()
 
     axes[1].bar(
         records["date"][:-1],
@@ -55,13 +53,13 @@ def show_plot(records: pd.DataFrame):
     )
     axes[1].plot(records["date"], records["projected_daily_consumed_units"], linestyle="--", label="Projected daily consumption")
     axes[1].plot(records["date"], records["average_daily_consumed_units"], label="Average daily consumption")
-    axes[1].legend()
 
     for axis in axes:
         axis.set_xlim(records["date"].min(), None)
         axis.set_ylim(0, None)
         axis.set_xlabel("Date / Time")
         axis.set_ylabel("Units")
+        axis.legend()
 
     figure.suptitle(
         f"{len(records)} record(s) from {records["date"].min().strftime(DATETIME_FORMAT)}"
@@ -73,11 +71,11 @@ def show_plot(records: pd.DataFrame):
 def main():
     latest_renewal_date = select(func.max(Record.renewal_date)).scalar_subquery()
     query = select(Record).where(Record.renewal_date == latest_renewal_date)
-    records = get_dataframe(query)
-    if records.empty:
-        print("No records were found.")
+    records_list = get_dataframes(query)
+    if records_list:
+        show_plot(records_list)
     else:
-        show_plot(records)
+        print("No records were found.")
 
 
 if __name__ == "__main__":
