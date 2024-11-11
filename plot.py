07b7e41693371186c3501ab3cd1a051d8a695a39
Fady Emad <fadyemad14705@gmail.com>
2024-11-11 15:00:48 +0200

    feat(plot): use pandas

--- plot.py
+++ plot.py
@@ -1,104 +1,83 @@
-from datetime import datetime
 from typing import Sequence, TypeVar, cast
 
 import matplotlib.pyplot as plot
-import numpy as np
+import pandas as pd
 from matplotlib.axes import Axes
 from matplotlib.colors import LinearSegmentedColormap, TwoSlopeNorm
 from matplotlib.figure import Figure
-from sqlalchemy import func, select
+from sqlalchemy import Select, func, select
 
-from icm.database import Session
+from icm.database import engine
 from icm.record import Record
 from icm.utility import DATETIME_FORMAT
 
 T = TypeVar("T")
 
 
-def show_plot(records: Sequence[Record]):
+def get_dataframe(query: Select[tuple[Record]]):
+    def calculate_average(record: pd.Series):
+        first = first_records.loc[record["renewal_date"]]
+        record["average_daily_consumed_units"] = (
+            record["consumed_units"] - first["consumed_units"]
+        ) / (first["days_left"] - record["days_left"])
+        return record
+
+    query = query.order_by(Record.date)
+    records = pd.read_sql(query, engine)
+    records["time_left"] = records["renewal_date"] - records["date"]
+    records["delta_consumed_units"] = records["consumed_units"].diff()
+    records["delta_date"] = records["date"].diff()
+    records["projected_delta_consumed_units"] = records["remaining_units"] * (records["delta_date"] / records["time_left"])
+    records["projected_delta_consumed_units_ratio"] = records["delta_consumed_units"] / records["projected_delta_consumed_units"]
+    records["days_left"] = records["time_left"] / pd.to_timedelta(1, "D")
+    records["projected_daily_consumed_units"] = records["remaining_units"] / records["days_left"]
+    first_records = records.groupby("renewal_date").first()
+    records = records.apply(calculate_average, axis=1)
+    return records
+
+
+def show_plot(records: pd.DataFrame):
     figure, axes = cast(tuple[Figure, Sequence[Axes]], plot.subplots(2, sharex=True))
     colormap = LinearSegmentedColormap.from_list("", ["green", "yellow", "red"], 16)
 
-    first_package_record: dict[datetime, Record] = {}
-    for record in records:
-        if (
-            record.renewal_date not in first_package_record
-            or record.date < first_package_record[record.renewal_date].date
-        ):
-            first_package_record[record.renewal_date] = record
+    axes[0].plot(records["date"], records["remaining_units"], color="blue", marker="o", label="Remaining units")
+    axes[0].legend()
 
-    dates = np.array([record.date for record in records], dtype=np.datetime64)
-    consumed_units = np.array([record.consumed_units for record in records], dtype=np.float64)
-    remaining_units = np.array([record.remaining_units for record in records], dtype=np.float64)
-    time_left = np.array([record.time_left for record in records], dtype=np.timedelta64)
-
-    timedeltas = np.ediff1d(dates)
-    consumed_since_last = np.ediff1d(consumed_units)
-    projected_consumption = remaining_units[:-1] * (timedeltas / time_left[:-1])
-    projected_consumption_daily = remaining_units / (time_left / np.timedelta64(1, "D"))
-    average_consumption_daily = (
-        consumed_units
-        - np.array(
-            [
-                first_package_record[record.renewal_date].consumed_units
-                for record in records
-            ],
-            dtype=np.float64,
-        )
-    ) / (
-        (
-            dates
-            - np.array(
-                [first_package_record[record.renewal_date].date for record in records],
-                dtype=np.datetime64,
-            )
-        )
-        / np.timedelta64(1, "D")
-    )
-
-    axes[0].plot(dates, remaining_units, color="blue", marker="o")
-    axes[0].set_ylabel("Remaining units")
     axes[1].bar(
-        dates[:-1],
-        consumed_since_last,
-        width=timedeltas,
-        color=colormap(TwoSlopeNorm(1, 0, 1.5)(consumed_since_last / projected_consumption)),
+        records["date"][:-1],
+        records["delta_consumed_units"][1:],
+        width=records["delta_date"][1:],
+        color=colormap(TwoSlopeNorm(1, 0, 1.5)(records["projected_delta_consumed_units_ratio"][1:])),
         align="edge",
         linewidth=1,
         edgecolor="white",
         label="Consumed units",
     )
-    axes[1].plot(dates, projected_consumption_daily, linestyle="--", label="Projected daily consumption")
-    axes[1].plot(dates, average_consumption_daily, color="red", label="Average daily consumption")
-    axes[1].set_ylabel("Units")
+    axes[1].plot(records["date"], records["projected_daily_consumed_units"], linestyle="--", label="Projected daily consumption")
+    axes[1].plot(records["date"], records["average_daily_consumed_units"], label="Average daily consumption")
     axes[1].legend()
 
     for axis in axes:
-        axis.set_xlim(dates.min(), None)
+        axis.set_xlim(records["date"].min(), None)
         axis.set_ylim(0, None)
-        axis.set_xlabel("Date and time")
+        axis.set_xlabel("Date / Time")
+        axis.set_ylabel("Units")
 
     figure.suptitle(
-        f"{len(records)} record(s) from {dates.min().astype(datetime).strftime(DATETIME_FORMAT)}"
-        f" to {dates.max().astype(datetime).strftime(DATETIME_FORMAT)}"
+        f"{len(records)} record(s) from {records["date"].min().strftime(DATETIME_FORMAT)}"
+        f" to {records["date"].max().strftime(DATETIME_FORMAT)}"
     )
     plot.show()
 
 
 def main():
-    with Session() as session:
-        latest_renewal_date = select(func.max(Record.renewal_date)).scalar_subquery()
-        records = list(
-            session.scalars(
-                select(Record)
-                .where(Record.renewal_date == latest_renewal_date)
-                .order_by(Record.date)
-            )
-        )
-        if records:
-            show_plot(records)
-        else:
-            print("No records were found.")
+    latest_renewal_date = select(func.max(Record.renewal_date)).scalar_subquery()
+    query = select(Record).where(Record.renewal_date == latest_renewal_date)
+    records = get_dataframe(query)
+    if records.empty:
+        print("No records were found.")
+    else:
+        show_plot(records)
 
 
 if __name__ == "__main__":
